29164e1b9c94de2971c25ff97250012f3b7ad61d
Asko Soukka <asko.soukka@iki.fi>
2025-09-28 13:03:50 +0300

    Add missing file

---
+++ src/lib/components/Quarter.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  export let color = "#000";
+  export let rotation = 0;
+</script>
+
+<style>
+  .quarter-container {
+    width: 100%;
+    height: 100%;
+    aspect-ratio: 1 / 1;
+  }
+</style>
+
+<div class="quarter-container" style="margin-top: -55%">
+  <svg
+    width="100%"
+    height="100%"
+    viewBox="0 0 100 100"
+    preserveAspectRatio="none"
+  >
+    <path
+      d="M 0,0 L 100,0 A 100,100 0 0,1 0,100 Z"
+      fill={color}
+      stroke="none"
+      transform="rotate({rotation * 90}, 50, 50)"
+    />
+  </svg>
+</div>
